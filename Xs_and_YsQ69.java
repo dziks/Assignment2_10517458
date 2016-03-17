633d9d1c1a00d7bd57f3c0906c4f14e955b3de05
francis dzikunu <francis dzikunu>
2016-03-17 14:46:34 +0000

    Assignment 2 submitted

---
+++ Xs_and_YsQ69.java
@@ -0,0 +1,16 @@
+
+
+/**
+ *
+ * @author Dziks
+ */
+public class Xs_and_YsQ69 {
+    public static void main(String[] args){
+        System.out.println("x \t\t y");
+        for(float x =-10; x<=10; x+=0.5 ){
+            System.out.print(x+"\t\t");
+            System.out.println(Math.pow(x,2));
+            
+        }
+    }
+}
